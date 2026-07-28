14c6a52114b7ace2feaa1f1e06fdb14c30d7399e
andrii-heorhitsa <georgica.andrii@gmail.com>
2026-07-28 14:10:41 +0300

    refactor: clean up default layout, fix flex chain and remove duplicate main

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,7 +2,7 @@
 
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { getQueryClient } from "../lib/get-query-client";
+import { getQueryClient } from "@/lib/get-query-client";
 import { ReactNode } from "react";
 
 export default function Providers({ children }: { children: ReactNode }) {
