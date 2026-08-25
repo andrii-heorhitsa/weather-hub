de9afaced69f2811d7929103746a8cb9b79f00a2
andrii-heorhitsa <georgica.andrii@gmail.com>
2026-08-25 12:45:02 +0300

    Added fix with GitHub secrets

--- src/lib/service/clothing-advice-service.ts
+++ src/lib/service/clothing-advice-service.ts
@@ -4,13 +4,19 @@ import { weatherCodeToText } from "../weather/weather-code-to-text";
 export async function getLLMAdvice(
   weather: CurrentWeatherDomain,
 ): Promise<string> {
+  const apiKey = process.env.GROQ_API_KEY;
+
+  if (!apiKey) {
+    throw new Error("GROQ_API_KEY is not set");
+  }
+
   const response = await fetch(
     "https://api.groq.com/openai/v1/chat/completions",
     {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        Authorization: `Bearer ${process.env.GROQ_API_KEY}`,
+        Authorization: `Bearer ${apiKey}`,
       },
       body: JSON.stringify({
         model: "openai/gpt-oss-20b",
